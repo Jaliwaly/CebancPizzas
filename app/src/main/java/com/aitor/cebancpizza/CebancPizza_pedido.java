32da16ff0009307cd7db9c0539f91266ccb7bf58
Inziel <ingaen93@gmail.com>
2017-01-07 14:59:10 +0100

    Pedido terminado, falta mapa, carrito y pequeños ajustes

--- app/src/main/java/com/aitor/cebancpizza/CebancPizza_pedido.java
+++ app/src/main/java/com/aitor/cebancpizza/CebancPizza_pedido.java
@@ -3,6 +3,7 @@ package com.aitor.cebancpizza;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,8 +17,8 @@ public class CebancPizza_pedido extends AppCompatActivity {
     private ArrayList<InformacionBebidas> bebidas;
     private InformacionCliente cliente;
     private ArrayList<InformacionPizza> pizzas;
-    private EditText texto;
-    private String contenido;
+    private TextView infoCliente,infoFactura,infoPrecio,infoTotal,infoRegalo,infoCantidad;
+    private String factura,precio,cantidad;
     private float total=0;
 
     @Override
@@ -29,12 +30,35 @@ public class CebancPizza_pedido extends AppCompatActivity {
         cliente = (InformacionCliente) datos.get(0).getObj();
         pizzas = (ArrayList<InformacionPizza>) datos.get(1).getObj();
         bebidas = (ArrayList<InformacionBebidas>) datos.get(2).getObj();
-        texto = (EditText) findViewById(R.id.txtResumen);
-        contenido="Información del cliente:\n\nNombre: "+cliente.getNombre()+"\nDirección: "+cliente.getDireccion()+"\nTeléfono: "+cliente.getTelefono()+"\n\nPedido:\n\n";
+        infoCliente = (TextView) findViewById(R.id.txtCliente);
+        infoFactura = (TextView) findViewById(R.id.txtArticulo);
+        infoPrecio = (TextView) findViewById(R.id.txtPrecio);
+        infoTotal = (TextView) findViewById(R.id.txtTotal);
+        infoRegalo = (TextView) findViewById(R.id.txtRegalos);
+        infoCantidad = (TextView) findViewById(R.id.txtCantidad);
+
+        infoCliente.setText("Información del cliente:\n\nNombre: "+cliente.getNombre()+"\nDirección: "+cliente.getDireccion()+"\nTeléfono: "+cliente.getTelefono()+"\n\n");
+        factura="Artículos pedidos:\n\n";
+        cantidad="Cantidad:\n\n";
+        precio="Precio:\n\n";
         for(int cont=0;cont<pizzas.size();cont++){
-            contenido+=pizzas.get(cont).getTipo()+" tamaño "+pizzas.get(cont).getTamano()+" "+pizzas.get(cont).getMasa()+" X "+pizzas.get(cont).getCantidad()+" -> "+pizzas.get(cont).getTotal()+"\n";
+            factura+=pizzas.get(cont).getTipo()+" tamaño "+pizzas.get(cont).getTamano()+" "+pizzas.get(cont).getMasa()+"\n";
+            cantidad+=pizzas.get(cont).getCantidad()+"\n";
+            precio+=pizzas.get(cont).getTotal()+"€\n";
             total+=pizzas.get(cont).getTotal();
         }
-        texto.setText(contenido);
+        for(int cont=0;cont<bebidas.size();cont++){
+            factura +=bebidas.get(cont).getTipo()+"\n";
+            cantidad+=bebidas.get(cont).getCantidad()+"\n";
+        }
+        infoFactura.setText(factura);
+        infoCantidad.setText(cantidad);
+        infoPrecio.setText(precio);
+        infoTotal.setText("Total: "+String.valueOf(total)+"€");
+        if(total>33)
+            infoRegalo.setText("Regalos especiales por su compra:\n\nPeluche Android\nVale para comer en el comedor de Cebanc");
+        else if(total >20)
+            infoRegalo.setText("Regalo especial por su compra:\n\nPeluche Android");
+
     }
 }
